ad024d089b2ebcf413451fe84a9112ae8a4944dc
Yoga Lahipa <dwiyogalahipa@gmail.com>
2020-05-16 12:11:48 +0700

    Add feature login

---
+++ src/Component/Member/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
+
+const RenderLogin = (props) => {
+    return (
+        <section>
+            START
+        </section>
+    )
+}
+
+export default RenderLogin;
